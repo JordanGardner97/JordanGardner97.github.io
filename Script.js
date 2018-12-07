80f9d264bd2fb26c221001b23eb2d46adfe889bc
JordanGardner97 <jordangardner20@gmail.com>
2018-12-07 10:44:36 +0000

    Cv upload

---
+++ Script.js
@@ -0,0 +1,43 @@
+function sendMessageFunction(){
+    alert("Your message has been sent");
+};
+
+
+var count = 0;
+
+
+function showMoreFunction(){
+if (count ==0)
+{
+    $('#CollegeResults').css("display","block");
+    count +=1;
+    $('.Show').text("Hide Grades")
+}
+
+else
+{
+    $('#CollegeResults').css("display","none");
+    count = 0;
+    $('.Show').text("Show Grades")
+}
+
+}
+
+var aboutMeCount = 0;
+
+function showMoreAboutMe(){
+    if (aboutMeCount ==0)
+    {
+        $('.hidAboutMeDiv').css("display","block");
+        aboutMeCount +=1;
+        $('.moreAboutMe').text("See Less")
+    }
+    
+    else
+    {
+        $('.hidAboutMeDiv').css("display","none");
+        aboutMeCount = 0;
+        $('.moreAboutMe').text("Show More")
+    }
+    
+    }
